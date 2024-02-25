74bbfeb774af9c496c3c0e41be9db58b9a93c465
CodeLordGh <babohkossi228@icloud.com>
2024-02-25 12:30:14 +0000

    vue expenses tracker

---
+++ src/components/AddTransition.vue
@@ -0,0 +1,30 @@
+<template>
+    <h3>Add new transaction</h3>
+    <form id="form" @submit.prevent="onSubmit">
+        <div class="form-controle">
+            <label for="text">Text</label>
+            <input type="text" id="text" v-model="text" placeholder="Enter text..." />
+        </div>
+        <div class="form-control">
+            <label for="amount">Amount
+                <br />
+                (negative - expense, positive - income)
+            </label>
+            <input type="text" id="amount" v-model="amount" placeholder="Enter amount..." />
+        </div>
+        <button class="btn">Add Transaction</button>
+    </form>
+</template>
+
+<script setup>
+
+import { ref } from 'vue';
+
+const text = ref('')
+const amount = ref('')
+
+const onSubmit = () => {
+    console.log(text.value, amount.value)
+
+}
+</script>
