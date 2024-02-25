0724cad5d2f8ea6f9b725c63cb956b7b1d8501d2
CodeLordGh <babohkossi228@icloud.com>
2024-02-25 15:54:07 +0000

    vue-expense-tracker-app logic completed

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <Header />
   <div class="container">
-    <Balance :total="total" />
+    <Balance :total="+total" />
     <IncomeExpenses :income="+income" :expenses="+expenses" />
-    <TransactionList :transactions="transactions" />
-    <AddTransition />
+    <TransactionList :transactions="transactions" @transactionDeleted="handleTransactionDeleted" />
+    <AddTransition @transactionSubmitted="handleTransactionSubmitted" />
   </div>
 </template>
 
@@ -14,29 +14,39 @@ import Balance from "./components/Balance.vue";
 import IncomeExpenses from "./components/IncomeExpenses.vue";
 import TransactionList from "./components/TransactionList.vue";
 import AddTransition from "./components/AddTransition.vue";
+import { useToast } from "vue-toastification";
 
-import { ref, computed } from "vue";
+import { ref, computed, onMounted } from "vue";
 
-const transactions = ref([
-  { id: 1, text: 'Flower', amount: -19.99 },
-  { id: 2, text: 'Salary', amount: 299.97 },
-  { id: 3, text: 'Book', amount: -10 },
-  // { id: 4, text: 'Bonus', amount: 500 }
-])
+onMounted(() => {
+  const savedTransactions = JSON.parse(localStorage.getItem("transactions"));
+
+  if (savedTransactions) {
+    transactions.value = savedTransactions;
+  }
+});
+
+const toast = useToast();
+
+const transactions = ref([]);
 // Get total
 const total = computed(() => {
-  return transactions.value.reduce((acc, transaction) => {
-    return acc + transaction.amount;
-  }, 0)
-})
+  return transactions.value
+    .reduce((acc, transaction) => {
+      return acc + transaction.amount;
+    }, 0)
+    .toFixed(2);
+});
 
 // Get income
 const income = computed(() => {
-  return transactions.value.filter((transaction) =>
-    transaction.amount > 0).reduce((acc, transaction) => {
+  return transactions.value
+    .filter((transaction) => transaction.amount > 0)
+    .reduce((acc, transaction) => {
       return acc + transaction.amount;
-    }, 0).toFixed(2)
-})
+    }, 0)
+    .toFixed(2);
+});
 
 //Get expenses
 const expenses = computed(() => {
@@ -45,7 +55,40 @@ const expenses = computed(() => {
     .reduce((acc, transaction) => {
       return acc + transaction.amount;
     }, 0)
-    .toFixed(2)
-})
+    .toFixed(2);
+});
 
+// Add transaction
+
+const handleTransactionSubmitted = (transactionData) => {
+  transactions.value.push({
+    id: generateUniqueId(),
+    text: transactionData.text,
+    amount: transactionData.amount,
+  });
+
+  savedTransactionsToLocalStorage();
+  toast.success("Transaction added!");
+};
+
+// Generate unique id
+const generateUniqueId = () => {
+  return Math.floor(Math.random() * 1000000);
+};
+
+// Delete Transaction
+const handleTransactionDeleted = (id) => {
+  transactions.value = transactions.value.filter(
+    (transaction) => transaction.id !== id
+  );
+
+  toast.success("Transaction deleted!");
+
+  toast.success("Transaction deleted!");
+};
+
+// Save to localStorage
+const savedTransactionsToLocalStorage = () => {
+  localStorage.setItem("transactions", JSON.stringify(transactions.value));
+};
 </script>
--- src/components/AddTransition.vue
+++ src/components/AddTransition.vue
@@ -1,30 +1,51 @@
 <template>
-    <h3>Add new transaction</h3>
-    <form id="form" @submit.prevent="onSubmit">
-        <div class="form-controle">
-            <label for="text">Text</label>
-            <input type="text" id="text" v-model="text" placeholder="Enter text..." />
-        </div>
-        <div class="form-control">
-            <label for="amount">Amount
-                <br />
-                (negative - expense, positive - income)
-            </label>
-            <input type="text" id="amount" v-model="amount" placeholder="Enter amount..." />
-        </div>
-        <button class="btn">Add Transaction</button>
-    </form>
+  <h3>Add new transaction</h3>
+  <form id="form" @submit.prevent="onSubmit">
+    <div class="form-controle">
+      <label for="text">Text</label>
+      <input type="text" id="text" v-model="text" placeholder="Enter text..." />
+    </div>
+    <div class="form-control">
+      <label for="amount"
+        >Amount
+        <br />
+        (negative - expense, positive - income)
+      </label>
+      <input
+        type="text"
+        id="amount"
+        v-model="amount"
+        placeholder="Enter amount..."
+      />
+    </div>
+    <button class="btn">Add Transaction</button>
+  </form>
 </template>
 
 <script setup>
+import { ref } from "vue";
+import { useToast } from "vue-toastification";
 
-import { ref } from 'vue';
+const text = ref("");
+const amount = ref("");
 
-const text = ref('')
-const amount = ref('')
+const emit = defineEmits(["transactionSubmitted"]);
+
+const toast = useToast();
 
 const onSubmit = () => {
-    console.log(text.value, amount.value)
+  if (!text.value || !amount.value) {
+    toast.error("Both field must be filled");
+    return;
+  }
 
-}
+  const transactionData = {
+    text: text.value,
+    amount: parseFloat(amount.value),
+  };
+
+  emit("transactionSubmitted", transactionData);
+  text.value = "";
+  amount.value = "";
+};
 </script>
